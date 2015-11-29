b19082cbdce8e366c39e1dc243e0316679f23b51
GPLme <524019351@qq.com>
2015-11-29 08:03:47 +0800

    删除了测试里俩没用到的变量

--- CharacterValidation/test/db-test.php
+++ CharacterValidation/test/db-test.php
@@ -132,37 +132,6 @@ function testEmbeddedQuestionMarks($MYSQL_CONNECTION_URL) {
     $db->executeSQL($sql);
     
     $event_id =  906300;
-    $event = array(
-        "event_id" => 906300,
-        "title" => "",
-        "subtitle" => "USC School of Cinematic Arts",
-        "summary" => "",
-        "description" => "",
-        "cost" => "Free",
-        "organizer" => "aago@cinema.usc.edu",
-        "contact_phone" => "(213) 740-2330",
-        "contact_email" => "",
-        "rsvp_email" => "",
-        "rsvp_url" => "http:\/\/cinema.usc.edu\/events\/reservation.cfm?id=13787",
-        "website_url" => "http:\/\/cinema.usc.edu\/events\/event.cfm?id=13787",
-        "ticket_url" => "",
-        "campus" => "",
-        "venue" => "The Albert and Dana Broccoli Theatre, SCA 112, George Lucas Buil",
-        "building_code" => "",
-        "room" => "SCA 112",
-        "address" => "",
-        "feature_candidate" => 0,
-        "user_id" => 0,
-        "username" => "guest",
-        "display_name" => "guest",
-        "scratch_pad" => "",
-        "created" => "2013-10-10 16:16:01",
-        "updated" => "2013-10-14 12:33:20",
-        "publication_date" => "0001-01-01 00:00:00",
-        "calendar_id" => 32,
-        "parent_calendar_id" => 32,
-        "parent_calendar" => "USC Public Events"
-	);
     $sql = 'INSERT INTO test_events (event_id, title, subtitle, summary, ' .
         'description, ' .
         'venue, campus, building_code, room, address, cost, ' .
@@ -172,40 +141,7 @@ function testEmbeddedQuestionMarks($MYSQL_CONNECTION_URL) {
         'scratch_pad, created, updated, publication_date) VALUES (' .
         '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, '. 
         '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ' .
-        '?, ?, ?, ?, ?, ?, ?)';
-    /*$isOK = $db->executeSQL($sql, array( //这玩意从来没被用到过
-        $event['event_id'],
-        $event['title'],
-        $event['subtitle'],
-        $event['summary'],
-        $event['description'],
-        $event['venue'],
-        $event['campus'],
-        $event['building_code'],
-        $event['room'],
-        $event['address'],
-        $event['cost'],
-        
-        $event['organizer'],
-        $event['contact_phone'],
-        $event['contact_email'],
-        $event['rsvp_email'],
-        $event['rsvp_url'],
-        $event['website_url'],
-        $event['ticket_url'],
-        $event['feature_candidate'],
-        $event['user_id'],
-        $event['username'],
-        
-        $event['display_name'],
-        $event['parent_calendar_id'],
-        $event['parent_calendar'],
-        $event['scratch_pad'],
-        $event['created'],
-        $event['updated'],
-        $event['publication_date']), true);*/
-
-    
+        '?, ?, ?, ?, ?, ?, ?)';   
     $sql = 'SELECT event_id FROM test_events WHERE event_id = ?';
     $db->executeSQL($sql, array($event_id));
     $row = $db->getRow();
